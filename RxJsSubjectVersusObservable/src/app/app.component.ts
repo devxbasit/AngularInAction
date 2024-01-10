01e6b2ac353ed6dc02ef8a3fdccb9e778cd6f337
Basit <devxbasit@gmail.com>
2024-01-11 00:33:28 +0530

    updated RxJs Example

--- RxJsSubjectVersusObservable/src/app/app.component.ts
+++ RxJsSubjectVersusObservable/src/app/app.component.ts
@@ -14,43 +14,63 @@ export class AppComponent implements OnInit {
   title = 'RxJsSubjectVersusObservable';
 
   ngOnInit(): void {
+    this.subjectDemo();
+    this.observableDemo();
+  }
+
+  subjectDemo() {
     const subject = new Subject<number>();
 
-    // subscriber 1
+    console.log('Subject - Registering Subscriber 1');
     subject.subscribe((data) => {
       console.log(`subject subscriber 1, data = ${data}`);
     });
 
-    // subscriber 2
+    console.log('Subject - Registering Subscriber 2');
     subject.subscribe((data) => {
       console.log(`subject subscriber 2, data = ${data}`);
     });
 
-    console.log('subject.next');
+    console.log('subject.next(x)');
     subject.next(Math.random());
 
-    console.log('subject.next');
+    console.log('subject.next(x)');
     subject.next(Math.random());
 
+    console.log('Subject - Registering Subscriber 3');
+    subject.subscribe((data) => {
+      console.log(`subject subscriber 3, data = ${data}`);
+    });
+
+    console.log('subject.next(x)');
+    subject.next(Math.random());
+  }
+
+  observableDemo() {
     const observable = new Observable<number>((observer) => {
-      console.log('observer.next');
+      console.log('observer.next(x)');
       observer.next(Math.random());
 
-      console.log('observer.next');
+      console.log('observer.next(x)');
       observer.next(Math.random());
     });
 
+    console.log('Observable - Registering Subscriber 1');
+    observable.subscribe((data) => {
+      console.log(`observable's subscriber 1, data = ${data}`);
+    });
+
     setTimeout(() => {
-      // subscriber 1
+      console.log('Observable - Registering Subscriber 2');
       observable.subscribe((data) => {
-        console.log(`observable subscriber 1, data = ${data}`);
+        console.log(`observable's subscriber 2, data = ${data}`);
       });
     }, 3000);
 
     setTimeout(() => {
-      // subscriber 1
+      console.log('Observable - Registering Subscriber 3');
       observable.subscribe((data) => {
-        console.log(`observable subscriber 2, data = ${data}`);
+        console.log(`observable's subscriber 3, data = ${data}`);
       });
     }, 6000);
   }
