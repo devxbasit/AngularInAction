4ece8e08a8eceb99c73a4205399663f7b7684716
Basit <devxbasit@gmail.com>
2024-08-18 01:31:40 +0530

    folder structure fix

--- NgRxInAction/counter-app/src/app/features/counter/state/counter.state.ts
+++
@@ -1,9 +0,0 @@
-export interface ICounterState {
-  counter: number;
-  channelName: string;
-}
-
-export const counterInitialState: ICounterState = {
-  counter: 5,
-  channelName: '#1 Main Channel',
-};
